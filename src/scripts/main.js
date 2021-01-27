e2625ec356e20b91fb7d54b0e4bf788ac3878e6c
Damian Soto <soto.platero@gmail.com>
2021-01-26 20:55:11 -0500

    add webmentions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,13 @@ import '../styles/tailwind.css';
 // import { handlePrivacyPolicyNoticeDismissal } from './privacy-policy.js';
 import { handleCodeCopying } from './copy.js';
 import handleDarkMode from './dark-mode.js';
+import handleWebmentions from './webmentions.js';
 
 if (DEV_MODE) console.log('Dev mode is currently enabled.');
 
 // handlePrivacyPolicyNoticeDismissal();
 handleCodeCopying();
 handleDarkMode();
+handleWebmentions()
 
 
